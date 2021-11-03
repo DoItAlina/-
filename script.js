657f7bb2136c7456bb493eacd8f3e451d9cdb1ae
Alina <freedomalina@mail.ru>
2021-11-03 23:20:49 +0500

    Added DOM instead of alert

--- script.js
+++ script.js
@@ -1,78 +1,87 @@
 
 
-    let num1, num2, result;
-    function plus() {
+let num1, num2, result;
+function plus() {
          
-         num1 = document.getElementById('n1').value;
-         num1 = +num1;
+     num1 = document.getElementById('n1').value;
+     num1 = +num1;
 
-         if (isNaN (num1) || isNaN(num2)) {alert ("ERROR");}
-         if (num1 == "" || num2 == "") { alert ("Введите число!");}
+     num2 = document.getElementById('n2').value;
+     num2 = +num2;
 
-         num2 = document.getElementById('n2').value;
-         num2 = +num2;
-
-         if (isNaN (num1) || isNaN(num2)) {alert ("Error");}
-         if (num1 == "" || num2 == "") { alert ("Введите число!");}
-
-
-         result = num1 + num2;
-         document.getElementById('out').innerHTML = result;
-    }
-    function minus() {
+     if (isNaN (num1) || isNaN(num2)) {
          
-         num1 = document.getElementById('n1').value;
-         num1 = +num1;
+      document.getElementById('out').innerHTML = 'ERROR';
+         return;
+     }
+     if (num1 === "" || num2 === "") { 
+      document.getElementById('out').innerHTML = 'Вы ничего не ввели!';
+          return;
+     }
+          
+     result = num1 + num2;
+     document.getElementById('out').innerHTML = result;
+}
+function minus() {
+         
+     num1 = document.getElementById('n1').value;
+     num1 = +num1;
 
-         if (isNaN (num1) || isNaN(num2)) {alert ("Error");}
-         if (num1 == "" || num2 == "") { alert ("Введите число!");}
+     num2 = document.getElementById('n2').value;
+     num2 = +num2;
 
-
-         num2 = document.getElementById('n2').value;
-         num2 = +num2;
-
-         if (isNaN (num1) || isNaN(num2)) {alert ("Error");}
-         if (num1 == "" || num2 == "") { alert ("Введите число!");}
-
-
-         result = num1 - num2;
-         document.getElementById('out').innerHTML = result;
-    }
+     if (isNaN (num1) || isNaN(num2)) {
+         
+      document.getElementById('out').innerHTML = 'ERROR';
+         return;
+     }
+     if (num1 === "" || num2 === "") { 
+      document.getElementById('out').innerHTML = 'Вы ничего не ввели!';
+          return;
+     }
+          
+     result = num1 - num2;
+     document.getElementById('out').innerHTML = result;
+}
     function mult() {
          
-         num1 = document.getElementById('n1').value;
-         num1 = +num1;
+     num1 = document.getElementById('n1').value;
+     num1 = +num1;
 
-         if (isNaN (num1) || isNaN(num2)) {alert ("Error");}
-         if (num1 == "" || num2 == "") { alert ("Введите число!");}
+     num2 = document.getElementById('n2').value;
+     num2 = +num2;
 
-
-         num2 = document.getElementById('n2').value;
-         num2 = +num2;
-
-         if (isNaN (num1) || isNaN(num2)) {alert ("Error");}
-         if (num1 == "" || num2 == "") { alert ("Введите число!");}
-
-
-         result = num1 * num2;
-         document.getElementById('out').innerHTML = result;
-    }
+     if (isNaN (num1) || isNaN(num2)) {
+         
+      document.getElementById('out').innerHTML = 'ERROR';
+         return;
+     }
+     if (num1 === "" || num2 === "") { 
+      document.getElementById('out').innerHTML = 'Вы ничего не ввели!';
+          return;
+     }
+          
+     result = num1 * num2;
+     document.getElementById('out').innerHTML = result;
+}
     function div() {
          
-         num1 = document.getElementById('n1').value;
-         num1 = +num1;
+     num1 = document.getElementById('n1').value;
+     num1 = +num1;
 
-         if (isNaN (num1) || isNaN(num2)) {alert ("Error");}
-         if (num1 == "" || num2 == "") { alert ("Введите число!");}
+     num2 = document.getElementById('n2').value;
+     num2 = +num2;
 
-
-         num2 = document.getElementById('n2').value;
-         num2 = +num2;
-
-         if (isNaN (num1) || isNaN(num2)) {alert ("Error");}
-         if (num1 == "" || num2 == "") { alert ("Введите число!");}
+     if (isNaN (num1) || isNaN(num2)) {
          
-         result = num1 / num2;
-         document.getElementById('out').innerHTML = result;
-    }
-    
+      document.getElementById('out').innerHTML = 'ERROR';
+         return;
+     }
+     if (num1 === "" || num2 === "") { 
+      document.getElementById('out').innerHTML = 'Вы ничего не ввели!';
+          return;
+     }
+          
+     result = num1 / num2;
+     document.getElementById('out').innerHTML = result;
+}
